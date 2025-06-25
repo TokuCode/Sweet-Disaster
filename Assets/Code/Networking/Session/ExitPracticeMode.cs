8b94262af67b7329a026e84db864d290efc0b2b2
iBraii <adrianblume1005@gmail.com>
2025-06-24 19:46:41 -0500

    exit practice mode

---
+++ Assets/Code/Networking/Session/ExitPracticeMode.cs
@@ -0,0 +1,23 @@
+using Code.Helpers.UI;
+using UnityEngine;
+
+namespace Code.Networking.Session
+{
+    public class ExitPracticeMode : MonoBehaviour
+    {
+        private bool _hasPressedEscape;
+        
+        private void Update()
+        {
+            if (!SessionManager.Instance.IsPracticeMode) return;
+            
+            if (_hasPressedEscape) return;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _hasPressedEscape = true;
+                SessionManager.Instance.LeaveSession();
+                UIUtilities.Instance.LoadScene("MainMenu");
+            }
+        }
+    }
+}
